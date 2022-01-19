90640417dcfb99aab74a3e2f2ac6a420505475df
frame88 <fleuzzi97@gmail.com>
2022-01-19 19:01:11 +0100

    starter pack

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Header />
+    <Main />
+    <Footer />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
+
+  <!-- Descrizione:
+Create un nuovo progetto utilizzando Vue CLI e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+Concentratevi poi sulla parte grafica e SCSS creando a mano i menu. -->
+
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+//import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+
+
+
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    //HelloWorld,
+    Header,
+    Main,
+    Footer,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/scss/style.scss"
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    {{ title }}
+  </footer>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    data() {
+      return {
+        title: 'Lorem'
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>D
