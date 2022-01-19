c1f602e186db4f737c1a8e3cbf7e382b370c0245
frame88 <fleuzzi97@gmail.com>
2022-01-19 19:56:35 +0100

    complete header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,10 @@
 <template>
   <header>
-    <h1>bell e buon</h1>
     <div class="container center">
-      <div class="left">
+      <div class="left-header">
         <img src="..\assets\img\dc-logo.png" alt="">
       </div>
-      <div class="right">
+      <div class="right-header">
         <ul>
       <li>
         <a href="">Characters</a>
@@ -50,13 +49,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  
   .container {
     display: flex;
-  }
+    background-color: aqua;
+  }  
 
-  .right {
+  .left-header {
     display: flex;
     align-items: center;
+    justify-content: center;
+    width: 20%;
+  }
+  
+  .right-header {
+    width: 80%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
   ul {
@@ -68,7 +78,16 @@ export default {
     li a {
       display: block;
       text-decoration: none;
-      margin: 0.3em;      
+      margin: 0.3em;
+      padding-bottom: 3em;
+      padding-top: 3em;
+      color: gray;
+      font-weight: bold;
+      &:hover,
+      &:active {
+        border-bottom: 4px solid blue;
+        color: blue;
+      }
     }
   }
 </style>
